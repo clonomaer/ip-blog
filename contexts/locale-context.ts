6123aa612f432aba7f45b23aa16f95c4b7b94287
clonomaer <clonomaer@gmail.com>
2022-02-23 13:54:21 +0330

    change locale load mechanics and add error handling

---
+++ contexts/locale-context.ts
@@ -0,0 +1,4 @@
+import { __ } from 'locales'
+import { from } from 'rxjs'
+
+export const localeContext$ = from(__())
--- hooks/locale.ts
+++ hooks/locale.ts
@@ -1,18 +1,34 @@
+import { config } from 'configs'
+import { flashToast$ } from 'contexts/flash-toast'
+import { localeContext$ } from 'contexts/locale-context'
 import { __ } from 'locales'
 import { WebsiteLocale } from 'locales/interface'
+import { useRouter } from 'next/router'
 import { useEffect } from 'react'
-import { useAsync } from './async'
+import { timer } from 'rxjs'
 import { useGetIsFirstRender } from './is-first-render'
+import { useObservable } from './observable'
 import { usePageLoadingStatus } from './page-loading-status'
+import { useSubscribe } from './subscribe'
 
-export function useLocale(): WebsiteLocale | undefined {
-    const locale = useAsync(__())
+export function useLocale(): WebsiteLocale | undefined | Error {
+    const locale = useObservable(localeContext$)
     const [_, addLoadingJob] = usePageLoadingStatus()
     const getIsFirstRender = useGetIsFirstRender()
+    const router = useRouter()
     useEffect(() => {
         if (getIsFirstRender()) {
             addLoadingJob(__())
         }
     }, [])
+    useSubscribe(localeContext$, {
+        error: e => {
+            console.error('Error loading locale', e)
+            flashToast$.next(
+                'There was an error loading locale files, page will refresh to retry.',
+            )
+            timer(config.Delays.confirm).subscribe(() => router.reload())
+        },
+    })
     return locale
 }
