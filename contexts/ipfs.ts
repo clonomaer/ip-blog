f235470227b214d8a64162703377a150ebad5ece
clonomaer <clonomaer@gmail.com>
2022-02-23 21:04:19 +0330

    wait for ipfs node to become online

--- contexts/ipfs.ts
+++ contexts/ipfs.ts
@@ -1,3 +1,23 @@
+import { config } from 'configs'
 import { create } from 'ipfs-core'
-import { from, shareReplay } from 'rxjs'
-export const ipfs$ = from(create()).pipe(shareReplay(1))
+import {
+    from,
+    mergeMap,
+    of,
+    retryWhen,
+    shareReplay,
+    throwError,
+    timer,
+} from 'rxjs'
+
+const ipfsNode$ = from(create()).pipe(shareReplay(1))
+ipfsNode$.subscribe({ complete: () => console.log('ipfs node created') })
+
+export const ipfs$ = ipfsNode$.pipe(
+    mergeMap(ipfs =>
+        ipfs.isOnline() ? of(ipfs) : throwError(() => 'ipfs is not ready'),
+    ),
+    retryWhen(() => timer(config.Retry.Timeout)),
+)
+
+ipfs$.subscribe({ complete: () => console.log('ipfs node online') })
