30fc5d7568000d0e022a1b39985f7974d1727415
clonomaer <clonomaer@gmail.com>
2022-02-25 18:05:02 +0330

    fix useOnce cleanup

--- hooks/once.ts
+++ hooks/once.ts
@@ -2,12 +2,13 @@ import { useEffect } from 'react'
 import { useLazyRef } from './lazy-ref'
 
 // this hook is to prevent HMR to run effects that have no dependencies more than once to ease development.
-export function useOnce(cb: () => void, cleanup?: () => void): void {
+export function useOnce(cb: () => (() => void) | void): void {
     const isDone = useLazyRef(() => false)
     useEffect(() => {
+        let cleanup: (() => void) | void
         if (!isDone.current && typeof window !== 'undefined') {
             isDone.current = true
-            cb()
+            cleanup = cb()
         }
         return cleanup
     }, [])
