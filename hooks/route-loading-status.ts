a2143ddf8fd874f586d7e281036f30621faa7821
clonomaer <clonomaer@gmail.com>
2022-02-25 20:22:38 +0330

    aggressively unregister event listeners on routes and add a minimum delay

--- hooks/route-loading-status.ts
+++ hooks/route-loading-status.ts
@@ -7,16 +7,40 @@ import { useOnce } from './once'
 export function useRouteLoadingStatus(): void {
     const router = useRouter()
     useOnce(() => {
-        router.events.on('routeChangeStart', () => {
+        let completeListeners: (() => void)[] = []
+        let errorListeners: (() => void)[] = []
+        function listener() {
             pageLoadingJobs$.next(
                 Promise.all([
                     new Promise<void>((resolve, reject) => {
-                        router.events.on('routeChangeComplete', () => resolve())
-                        router.events.on('routeChangeError', () => reject())
+                        completeListeners.push(function () {
+                            resolve()
+                        })
+                        errorListeners.push(function () {
+                            reject()
+                        })
+                        router.events.on(
+                            'routeChangeComplete',
+                            completeListeners[completeListeners.length - 1]!,
+                        )
+                        router.events.on(
+                            'routeChangeError',
+                            errorListeners[errorListeners.length - 1]!,
+                        )
                     }),
                     waitFor(config.Delays.min),
                 ]),
             )
-        })
+        }
+        router.events.on('routeChangeStart', listener)
+        return () => {
+            router.events.off('routeChangeStart', listener)
+            completeListeners.forEach(f =>
+                router.events.off('routeChangeComplete', f),
+            )
+            errorListeners.forEach(f =>
+                router.events.off('routeChangeError', f),
+            )
+        }
     })
 }
