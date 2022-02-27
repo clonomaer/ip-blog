792adb806bd1f17b65df468ac0138054343fd5d3
clonomaer <clonomaer@gmail.com>
2022-02-27 16:12:43 +0330

    make sure web3 provider id is replayed

--- observables/web3-provider-id.ts
+++ observables/web3-provider-id.ts
@@ -3,13 +3,26 @@ import { flashToast$ } from 'contexts/flash-toast'
 import { localCache } from 'contexts/local-cache'
 import { isWeb3ProviderId } from 'helpers/validate-web3-provider'
 import { __$ } from 'locales'
-import { asyncScheduler, EMPTY, filter, mergeMap, of, tap, timer } from 'rxjs'
+import {
+    asyncScheduler,
+    EMPTY,
+    filter,
+    mergeMap,
+    of,
+    shareReplay,
+    tap,
+    timer,
+} from 'rxjs'
 import { Web3ProviderId } from 'types'
 import { noSentinelOrUndefined } from 'utils/no-sentinel-or-undefined'
 
 export const Web3ProviderId$ = localCache
     .observe<string | undefined>(config.Web3ProviderIdCacheKey)
-    .pipe(filter(noSentinelOrUndefined), filter(isWeb3ProviderId))
+    .pipe(
+        filter(noSentinelOrUndefined),
+        filter(isWeb3ProviderId),
+        shareReplay(1),
+    )
 
 const validProvider: { last: Web3ProviderId | undefined } = {
     last: undefined,
