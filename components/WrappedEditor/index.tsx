4bc03bf30f96298029786297b28d24ce31e989d4
clonomaer <clonomaer@gmail.com>
2022-02-27 13:44:53 +0330

    less hassle with big screens and the scale factor on viewer

--- components/WrappedEditor/index.tsx
+++ components/WrappedEditor/index.tsx
@@ -6,6 +6,7 @@ import Editor, { Props as EditorProps } from 'rich-markdown-editor'
 import { fromEvent, throttleTime } from 'rxjs'
 import { useSubscribe } from 'hooks/subscribe'
 import { useMeasure } from 'hooks/measure'
+import { useUntil } from 'hooks/responsive'
 
 export type WrappedEditorProps = {
     className?: ClassName
@@ -17,6 +18,8 @@ export default function WrappedEditor({
     ...props
 }: WrappedEditorProps): React.ReactElement | null {
     const [shouldExpand, setShouldExpand] = useState<boolean>(false)
+    const isMobile = useUntil('sm')
+    console.log('isMobile', isMobile)
     const ref = useRef<Editor>()
     useSubscribe(
         () =>
@@ -34,12 +37,14 @@ export default function WrappedEditor({
             const threshold = viewPortHeight / 3
             setShouldExpand(
                 (readOnly ?? false) &&
+                    (isMobile ?? false) &&
                     eHeight - 2 * threshold > 2 * viewPortHeight && // is worth doing the transition
                     Math.abs(eY) > threshold && // lower than the top threshold
                     eBottom > viewPortHeight + threshold && // higher than the bottom threshold
                     scrollTop > 0, // obvious but it fixes a bug with eY being reported wrong
             )
         },
+        [isMobile],
     )
     return (
         <Editor
@@ -51,13 +56,15 @@ export default function WrappedEditor({
                 'rounded-2xl',
                 'revert-tailwind-preflight',
                 'transition-all duration-1000',
-                'border-primary-dark',
                 'pl-4 pr-3 md:px-7 py-5 ',
                 'border',
                 styles.shouldExpand,
                 shouldExpand
-                    ? cn('bg-page-bg shadow-none', styles.expanded)
-                    : 'shadow-strong bg-opacity-10 bg-indigo-800',
+                    ? cn(
+                          'bg-page-bg shadow-none border-transparent',
+                          styles.expanded,
+                      )
+                    : 'shadow-strong bg-opacity-10 bg-indigo-800 border-primary-dark',
                 styles.container,
                 className,
             )}
