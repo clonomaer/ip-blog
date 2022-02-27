513458fdfc74b78bd953dbb881615a636d3fb1db
clonomaer <clonomaer@gmail.com>
2022-02-27 12:44:54 +0330

    better typegaurd on controlStreamPayload operator

--- operators/control-stream-payload.ts
+++ operators/control-stream-payload.ts
@@ -1,12 +1,15 @@
 import { SENTINEL } from 'contexts/empty-sentinel'
 import _ from 'lodash'
 import { filter, map, OperatorFunction, tap } from 'rxjs'
+import { NonUndefinable } from 'types'
 import { noSentinelOrUndefined } from 'utils/no-sentinel-or-undefined'
 
 export function controlStreamPayload<
     Action extends keyof Payloads,
     Payloads extends _.Dictionary<unknown>,
->(action: Action): OperatorFunction<Payloads, Payloads[Action]> {
+>(
+    action: Action,
+): OperatorFunction<Payloads, NonUndefinable<Payloads[Action]>> {
     return source =>
         source.pipe(
             map(x => (action in x ? x[action] : SENTINEL)),
