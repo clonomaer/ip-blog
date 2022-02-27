834a8b0b956b5834c88639045ead85a0439bdbd4
clonomaer <clonomaer@gmail.com>
2022-02-27 07:38:54 +0330

    make darkreader ignore this website

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,10 @@ class MyDocument extends Document {
             <Html style={{ backgroundColor: 'hsl(120,40%,5%)' }}>
                 <Head>
                     <meta name="theme-color" content="#0e1f2f" />
+                    <meta
+                        name="darkreader"
+                        content="ee8a9ee6-93a6-427a-87dc-5936040ec8a6"
+                    />
                 </Head>
                 <div
                     className="font-serif"
