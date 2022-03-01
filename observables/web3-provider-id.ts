327ea3ffe3379167b1ea21a14a87c886e058dbc1
clonomaer <clonomaer@gmail.com>
2022-03-01 10:40:03 +0330

    turn web3 provider id into a subject

--- observables/web3-provider-id.ts
+++ observables/web3-provider-id.ts
@@ -2,9 +2,22 @@ import { config } from 'configs'
 import { localCache } from 'contexts/local-cache'
 import { isWeb3ProviderId } from 'helpers/validate-web3-provider'
 import { __$ } from 'locales'
-import { filter, map } from 'rxjs'
-import { noSentinelOrUndefined } from 'utils/no-sentinel-or-undefined'
+import { distinctUntilChanged, filter, map, ReplaySubject } from 'rxjs'
+import { Web3ProviderId } from 'types'
 
-export const Web3ProviderId$ = localCache
+export const Web3ProviderId$ = new ReplaySubject<Web3ProviderId | undefined>(1)
+
+localCache
     .observe<string | undefined>(config.Web3ProviderIdCacheKey)
-    .pipe(map(x => (x && isWeb3ProviderId(x) ? x : undefined)))
+    .pipe(
+        map(x => (x && isWeb3ProviderId(x) ? x : undefined)),
+        distinctUntilChanged(),
+    )
+    .subscribe(Web3ProviderId$)
+
+Web3ProviderId$.pipe(
+    map(x => (x && isWeb3ProviderId(x) ? x : undefined)),
+    distinctUntilChanged(),
+).subscribe(
+    localCache.observe<string | undefined>(config.Web3ProviderIdCacheKey),
+)
