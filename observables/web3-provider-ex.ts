165dea63a10830da33e1c12e5d1bc87c29e87078
clonomaer <clonomaer@gmail.com>
2022-03-01 10:36:21 +0330

    nit

--- observables/web3-provider-ex.ts
+++ observables/web3-provider-ex.ts
@@ -1,22 +1,22 @@
 import { config } from 'configs'
 import { ethers } from 'ethers'
-import { map, mergeMap, of, shareReplay } from 'rxjs'
+import { map, mergeMap, Observable, of } from 'rxjs'
 import { Web3ProviderEx } from 'types'
 import { Web3ProviderId$ } from './web3-provider-id'
 
-export const Web3ProviderEx$ = Web3ProviderId$.pipe(
-    mergeMap(id =>
-        !id
-            ? of(undefined)
-            : config.Web3Providers[id].provider$.pipe(
-                  map(externalProvider => {
-                      const providerEx = new ethers.providers.Web3Provider(
-                          externalProvider,
-                      )
-                      ;(providerEx as Web3ProviderEx).external =
-                          externalProvider
-                      return providerEx as Web3ProviderEx
-                  }),
-              ),
-    ),
-)
+export const Web3ProviderEx$: Observable<Web3ProviderEx | undefined> =
+    Web3ProviderId$.pipe(
+        mergeMap(id =>
+            !id ? of(undefined) : config.Web3Providers[id].provider$,
+        ),
+        map(externalProvider => {
+            if (!externalProvider) {
+                return undefined
+            }
+            const providerEx = new ethers.providers.Web3Provider(
+                externalProvider,
+            )
+            ;(providerEx as Web3ProviderEx).external = externalProvider
+            return providerEx as Web3ProviderEx
+        }),
+    )
