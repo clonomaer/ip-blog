5312a93971429d29d41b85198d96943bb7f2ac43
clonomaer <clonomaer@gmail.com>
2022-03-03 03:04:24 +0330

    recent posts section

---
+++ components/RecentPosts/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import cn from 'classnames'
+import { ClassName } from 'types'
+import { useObservable } from 'hooks/observable'
+import { recentPosts$ } from 'contexts/recent-posts'
+import Fallback from 'components/Fallback'
+import { shortenString } from 'utils/shorten-string'
+import { useRoutePush } from 'hooks/route-push'
+
+export type RecentPostsProps = {
+    className?: ClassName
+}
+
+export default function RecentPosts({
+    className,
+}: RecentPostsProps): React.ReactElement | null {
+    const postList = useObservable(recentPosts$)
+    const push = useRoutePush()
+    return (
+        <div
+            className={cn(
+                'flex flex-col border border-primary rounded-lg p-3 items-center',
+                className,
+            )}>
+            <div className="w-full text-center mb-4">recent posts</div>
+            <div className="w-full border-b border-primary mb-4" />
+            <div className="flex flex-col mb-2">
+                {postList instanceof Error ? (
+                    <Fallback />
+                ) : postList ? (
+                    postList.map((cid, index) => (
+                        <a
+                            className="flex transition-all hover:text-primary cursor-pointer active:text-primary-dark"
+                            onClick={push(`/post?postId=${cid}`)}>
+                            <i className="uil-file-alt text-xl mr-1" />
+                            {shortenString(cid, 6)}
+                        </a>
+                    ))
+                ) : (
+                    <div>you haven't posted anything yet!</div>
+                )}
+            </div>
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ import { InputControl } from 'components/InputWrapper'
 import { getSubjectValue } from 'utils/get-subject-value'
 import { isIPFS } from 'ipfs-core'
 import { useRoutePush } from 'hooks/route-push'
+import RecentPosts from 'components/RecentPosts'
 
 const Home: NextPage = ({}) => {
     const __ = useLocale()
@@ -22,40 +23,45 @@ const Home: NextPage = ({}) => {
 
     return (
         <div className="flex flex-col justify-center items-center overflow-auto">
-            <div className="flex flex-col items-center space-y-5">
-                <Button className="w-72 mb-20" job={push('new')}>
+            <div className="flex flex-col items-center space-y-20">
+                <Button className="w-72" job={push('new')}>
                     <i className="uil-file-plus-alt mr-2" />
                     {__?.landing.newPost}
                 </Button>
-                <div>{__?.landing.findCid}</div>
-                <div className="flex children:flex-grow w-full items-start">
-                    <Input
-                        control={cidInput$}
-                        validator={pipe(
-                            map(x =>
-                                isIPFS.cid(x)
-                                    ? ''
-                                    : __?.landing.wrongCid ?? 'wrong CID',
-                            ),
-                        )}
-                        sanitizer={pipe(map(x => x.replace(/ */g, '')))}
-                        label={__?.landing.Cid ?? ''}
-                        spellCheck={false}
-                        className={{
-                            outer: 'w-full',
-                            wrapper: 'w-full',
-                            target: 'w-full',
-                        }}
-                    />
-                    <Button
-                        className="!mt-0 !mr-0 !py-0 !px-2  h-10"
-                        disabled={
-                            !cidInput.Value?.length ||
-                            !_.isEmpty(cidInput.Error)
-                        }
-                        job={push(`/post?postId=${cidInput.Value ?? ''}`)}>
-                        <i className="uil-angle-right-b text-2xl" />
-                    </Button>
+                <div className="flex flex-col items-center space-y-3">
+                    <div>{__?.landing.findCid}</div>
+                    <div className="flex children:flex-grow w-full items-start">
+                        <Input
+                            control={cidInput$}
+                            validator={pipe(
+                                map(x =>
+                                    isIPFS.cid(x)
+                                        ? ''
+                                        : __?.landing.wrongCid ?? 'wrong CID',
+                                ),
+                            )}
+                            sanitizer={pipe(map(x => x.replace(/ */g, '')))}
+                            label={__?.landing.Cid ?? ''}
+                            spellCheck={false}
+                            className={{
+                                outer: 'w-full',
+                                wrapper: 'w-full',
+                                target: 'w-full',
+                            }}
+                        />
+                        <Button
+                            className="!mt-0 !mr-0 !py-0 !px-2  h-10"
+                            disabled={
+                                !cidInput.Value?.length ||
+                                !_.isEmpty(cidInput.Error)
+                            }
+                            job={push(`/post?postId=${cidInput.Value ?? ''}`)}>
+                            <i className="uil-angle-right-b text-2xl" />
+                        </Button>
+                    </div>
+                </div>
+                <div>
+                    <RecentPosts className="w-72" />
                 </div>
             </div>
         </div>
