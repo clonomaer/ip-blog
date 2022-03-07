1496e045e651295236e559ea5e9c568da0ab444b
clonomaer <clonomaer@gmail.com>
2022-03-07 23:47:20 +0330

    nit distinct changes

--- observables/wallet-connect-status.ts
+++ observables/wallet-connect-status.ts
@@ -1,8 +1,12 @@
-import { combineLatest, map } from 'rxjs'
+import { combineLatest, distinctUntilChanged, map, shareReplay } from 'rxjs'
 import { SignerAddress$ } from './signer-account'
 import { Web3ProviderId$ } from './web3-provider-id'
 
 export const WalletConnectStatus$ = combineLatest([
     Web3ProviderId$,
     SignerAddress$,
-]).pipe(map(([id, address]) => !!(id && address)))
+]).pipe(
+    map(([id, address]) => !!(id && address)),
+    distinctUntilChanged(),
+    shareReplay(1),
+)
