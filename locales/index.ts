f3eb95436093b77f5f156247b3ea18778e8a0b83
clonomaer <clonomaer@gmail.com>
2022-03-08 01:56:20 +0330

    nit remove logs and more convinient way to wait for next eventloop

--- locales/index.ts
+++ locales/index.ts
@@ -1,11 +1,13 @@
 import { WebsiteLocale, WebsiteLocaleData } from './interface'
 import { sep } from 'path'
 import {
+    asapScheduler,
     BehaviorSubject,
     catchError,
     map,
     mergeMap,
     mergeMapTo,
+    observeOn,
     of,
     ReplaySubject,
     tap,
@@ -20,7 +22,6 @@ export const locale$ = new BehaviorSubject<string>('en')
 
 locale$
     .pipe(
-        tap(x => console.log('locale shit', x)),
         mergeMap(_locale => import(`.${sep}${_locale}`)),
         map(x => x.default),
         mergeMap(x =>
@@ -40,12 +41,8 @@ locale$
                 }, trying to load fallback locale`,
             )
             locale$.next('en')
-            return timer(1000).pipe(mergeMapTo(observable))
+            return observable.pipe(observeOn(asapScheduler))
         }),
         map(x => x.data),
     )
     .subscribe(__$)
-
-__$.subscribe(x =>
-    console.log('more locale shit', x.userInteraction.confirmation.confirm),
-)
